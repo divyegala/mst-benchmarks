c519f1fe7dc57b55c4c157645358035246265ff3
divyegala <divyegala@gmail.com>
2023-01-30 13:26:23 -0800

    finalize conda env name

--- run_benchmarks.sh
+++ run_benchmarks.sh
@@ -12,11 +12,11 @@ done
 # running rapids benchmarks
 cd cugraph
 # get cugraph dev env, useful later as well
-# conda env create -n rapids-bench python=3.8 --file=conda/environments/cugraph_dev_cuda11.5.yml
-conda run -n rapids-bench-1 bash build.sh --skip_cpp_tests --without_cugraphops
+conda env create -n rapids-bench python=3.8 --file=conda/environments/cugraph_dev_cuda11.5.yml
+conda run -n rapids-bench bash build.sh --skip_cpp_tests --without_cugraphops
 for road in ${road_datasets[@]}; do
     echo ${road} >> ../out.txt
-    conda run -n rapids-bench-1 python run_rapids_bench.py "../datasets/${road}" | grep -E "RAPIDS Finished in: [0-9]+" >> ../out.txt
+    conda run -n rapids-bench python run_rapids_bench.py "../datasets/${road}" | grep -E "RAPIDS Finished in: [0-9]+" >> ../out.txt
 done
 cd ..
 
